312b3b26eff3f11a68e4a6de52c8303f7d783827
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-06-24 13:58:05 -0300

    first commit

---
+++ src/main/java/br/com/java/sistemacontrolecarros/SistemacontrolecarrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.java.sistemacontrolecarros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemacontrolecarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemacontrolecarrosApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/java/sistemacontrolecarros/controllers/ExampleController.java
@@ -0,0 +1,18 @@
+package br.com.java.sistemacontrolecarros.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/example")
+public class ExampleController {
+
+    @GetMapping("/hello-world")
+    public ResponseEntity<String> get() {
+
+        return ResponseEntity.ok("Olá Mundo");
+    }
+    
+}
